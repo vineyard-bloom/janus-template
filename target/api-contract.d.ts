d05944c56e22d2f80748822987bc76608df13dfa
Aaron Greenspan <afg419@gmail.com>
2018-02-08 12:35:27 -0700

    removes target

--- target/api-contract.d.ts
+++
@@ -1,8 +0,0 @@
-import {
-	CreateUserRequest, 
-	CreateUserResponse 
-} from '/Users/aarongreenspan/IbN/vineyard/vineyard-contract-template/target/endpoint-types'
-
-export interface ApiActions {
-  createUser: (req: CreateUserRequest) => Promise<CreateUserResponse>
-}
